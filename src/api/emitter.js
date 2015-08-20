a23ce825990d9fb43ac91561c954dc81692957f7
Patrick Eifler <pe@neopoly.de>
2015-08-20 23:24:35 +0200

    Refactor ajax loader

--- src/api/emitter.js
+++ src/api/emitter.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var Backbone = require("backbone"),
     $ = require("jquery"),
     L = require("lodash"),
--- src/utils/ajax_loader.js
+++ src/utils/ajax_loader.js
@@ -2,16 +2,16 @@ var $ = require("jquery"),
     L = require("lodash");
 
 var DOM = {
-    neo_scope: ".neo .neo-inner-content",
-    ajax_loader: ".neo_ajax_loader",
-    lock_until_loaded: ".lock_until_loaded"
+    scope: "body",
+    ajax_loader: ".loader",
+    lock_ui: ".lock-ui"
   },
   selector = {
     $loader: undefined,
     $locker: undefined
   },
-  loader_tmpl = '<div class="neo_ajax_loader"><div class="loader-gif"></div></div>',
-  locker_tmpl = '<div class="lock_until_loaded"></div>';
+  loader_tmpl = '<div class="loader"><div class="loader-gif"></div></div>',
+  locker_tmpl = '<div class="lock-ui"></div>';
 
 
 var setSelectors = function() {
@@ -19,28 +19,34 @@ var setSelectors = function() {
     selector.$loader = $(DOM.ajax_loader);
   }
   if(typeof selector.$locker === "undefined") {
-    selector.$locker = $(DOM.neo_scope).children(DOM.lock_until_loaded);
+    selector.$locker = $(DOM.scope).children(DOM.lock_ui);
   }
+};
+
+var init = function() {
+  $(DOM.scope).prepend(loader_tmpl);
+  $(DOM.scope).append(locker_tmpl);
+  $(DOM.scope).children(DOM.ajax_loader).hide();
+  $(DOM.scope).children(DOM.lock_ui).hide();
+
+  var onScroll = L.throttle(function(){
+    var scrollPos = $(window).scrollTop();
+    $(DOM.ajax_loader).css("padding-top", (scrollPos - 100) + "px");
+  });
+
+  var $doc = $(document);
+  $doc.scroll(onScroll);
+
+  onScroll();
 };
 
 $(function() {
   L.once(function(){
-    $(DOM.neo_scope).first().prepend(loader_tmpl);
-    $(DOM.neo_scope).first().append(locker_tmpl);
-    $(DOM.neo_scope).children(DOM.ajax_loader).hide();
-    $(DOM.neo_scope).children(DOM.lock_until_loaded).hide();
-
-    var onScroll = L.throttle(function(){
-      var scrollPos = $(window).scrollTop();
-      $(DOM.ajax_loader).css("padding-top", (scrollPos - 100) + "px");
-    });
-
-    var $doc = $(document);
-    $doc.scroll(onScroll);
-
-    onScroll();
+   init();
   });
 });
+
+exports.init = init;
 
 exports.showLoader = function () {
   setSelectors();
